62f7a3ff27738b64fa327ddab3752d35be2eaad5
Jonathan Kulak <lackadaize@msn.com>
2024-07-15 10:24:27 -0400

    minusBtn and plusBtn working properly.

--- js/challenge.js
+++ js/challenge.js
@@ -9,28 +9,40 @@ let count = 0
 let countInterval
 let paused = false
 
+// Counter Functions
 function counterBox () {
+    // Scoped Variables
     const counter = document.getElementById("counter")
     count = counter.textContent
-
-    function countInterval () {
-        count++
-        counter.textContent = count
-    }
-
-    function startCount () {
-        countInterval = interval(() => {
-            if (!paused) {
-                countInterval()
-            }
-        }, 1000)
-    }
     const minusBtn = document.getElementById("minus")
     const plusBtn = document.getElementById("plus")
     const heartBtn = document.getElementById("heart")
     const pauseBtn = document.getElementById("pause")
+
+    // Initial Count
+    function countInterval () {
+        count++
+        counter.textContent = count
+    }
+    setInterval(countInterval, 1000);
+
+    // Button Functions
+    function countMinus () {
+        count--
+        counter.textContent = count
+    }
+
+    function countPlus () {
+        count++
+        counter.textContent = count
+    }
+
+    // Event Listeners
+    minusBtn.addEventListener("click", countMinus)
+    plusBtn.addEventListener("click", countPlus)
 }
 
+// Comment Functions
 function commentForm() {
     document.getElementById('comment-form').addEventListener('submit', (e) => {
         e.preventDefault()
@@ -44,7 +56,6 @@ function commentForm() {
 }
 
 /*
-See the timer increment every second once the page has loaded.
 Manually increment and decrement the counter using the plus and minus buttons.
 "Like" an individual number of the counter. I should see the count of the number of "likes" associated with that number displayed.
 Pause the counter, which should:
@@ -52,5 +63,4 @@ pause the counter
 disable all buttons except the pause button
 switch the label on the button from "pause" to "resume"
 Click the "resume" button to restart the counter and re-enable the buttons.
-Leave comments on my gameplay, such as: "Wow, what a fun game this is."
 */
